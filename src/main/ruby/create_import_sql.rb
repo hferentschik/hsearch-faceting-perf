ce626d5eac7079b7841dcaca8986b5f2429012f4
Hardy Ferentschik <hardy@ferentschik.de>
2014-02-21 21:21:36 +0100

    Improving ruby scripts

--- src/main/ruby/create_import_sql.rb
+++ src/main/ruby/create_import_sql.rb
@@ -1,12 +1,57 @@
 require 'json'
 
+BOOK_INSERT_SQL = "insert into Book (id, isbn, title, publisher) values (%{book_id}, %{isbn}, '%{title}', '%{publisher}');\n"
+AUTHOR_INSERT_SQL = "insert into Author (id, name) values (%{author_id}, '%{name}');\n"
+BOOK_AUTHOR_SQL = "insert into Book_Author (Book_id, authors_id) values (%{book_id}, %{author_id});\n"
+
+def get_author_id(author_string_id)
+  if @author_id_to_int_id[author_string_id].nil?
+    @author_id_to_int_id[author_string_id] = @author_id
+  end
+  @author_id_to_int_id[author_string_id]
+end
+
+@author_id = 0
+@book_id = 0
+
+@book_inserts = Array.new
+@author_inserts = Array.new
+@book_author_inserts = Array.new
+
+@book_id_to_author_id = Hash.new
+@author_id_to_int_id = Hash.new
+
 data = JSON.parse(IO.read("books.json"))
 data.each do |book|
-  puts "#{book['title']}"
-  puts "#{book['publisher_name']}"
+  values = {:book_id => @book_id, :isbn => book['isbn13'], :title => book['title'], :publisher => book['publisher_text']}
+  @book_inserts << BOOK_INSERT_SQL % values
+  @book_id += 1
+
   book['author_data'].each do |author|
-    puts "#{author['name']}"
+    author_id = get_author_id author['id']
+    if author_id == @author_id
+      values = {:author_id => author_id, :name => author['name']}
+      @author_inserts << AUTHOR_INSERT_SQL % values
+      @author_id += 1
+    end
+    @book_id_to_author_id[@book_id] = author_id
   end
-  puts ">>>"
-  puts ""
 end
+
+@book_id_to_author_id.each do |key, value|
+  values = {:book_id => key, :author_id => value}
+  @book_author_inserts << BOOK_AUTHOR_SQL % values
+end
+
+File.open('import.sql', 'w') do |f|
+  f.puts @book_inserts
+  f.puts @author_inserts
+  f.puts @book_author_inserts
+end
+
+
+
+
+
+
+
